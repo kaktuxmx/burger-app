3435d0b9ed6bcd4facd6a3aff66889226b9ced54
kaktuxmx <kaktuxsql@gmail.com>
2018-04-13 18:44:15 -0700

    Merge server and models

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@
 use burger;
 
 create table burger (
-    burger_id INT not null primary key AUTO_INCREMENT,
-    burger_name VARCHAR (250) not null,
+    burger_id INT not null primary key auto_increment,
+    burger_name VARCHAR not null,
     gone_burguer BOOLEAN default false    
 );
